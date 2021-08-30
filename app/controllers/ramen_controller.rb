f2316d6a710fb8b3c62703c6597326c37fac319e
keita12 <18b1228@andrew.ac.jp>
2021-08-30 18:30:55 +0900

    Basic認証を導入

--- app/controllers/ramen_controller.rb
+++ app/controllers/ramen_controller.rb
@@ -1,6 +1,10 @@
 class RamenController < ApplicationController
+	before_action :authenticate_user!, except: [:index, :show]
+	before_action :set_ramen, only: [:show, :edit, :update,:destroy]
+	before_action :move_to_edit, only: [:edit,:update]
+
 	def index
-		@ramen = Ramen.all
+		@ramen = Ramen.all.order("created_at DESC")
 	end
 	def new
 		@ramen = Ramen.new
@@ -14,15 +18,12 @@ class RamenController < ApplicationController
     end
 	end
 	def show
-		@ramen = Ramen.find(params[:id])
 		@comment = Comment.new
 		@comments = @ramen.comments.includes(:user)
 	end
 	def edit
-		@ramen = Ramen.find(params[:id])
 	end
 	def update
-		@ramen = Ramen.find(params[:id])
 		if @ramen.update(ramen_params)
 			redirect_to raman_path
 		else
@@ -30,12 +31,19 @@ class RamenController < ApplicationController
 		end
 	end
 	def destroy
-		@ramen = Ramen.find(params[:id])
 		@ramen.destroy
 		redirect_to root_path
 	end
 	private
 	def ramen_params
 		params.require(:ramen).permit(:store_name,:ramen_name,:star_id, :image,:text).merge(user_id: current_user.id)
+	end
+	def set_ramen
+		@ramen = Ramen.find(params[:id])
+	end
+	def move_to_edit
+    unless current_user.id == @ramen.user_id
+      redirect_to root_path
+    end
 	end
 end
