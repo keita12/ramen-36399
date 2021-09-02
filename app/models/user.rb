893b55d81deb3e8a36fcc66d32016839b97eea8b
keita12 <18b1228@andrew.ac.jp>
2021-09-02 16:45:05 +0900

    いいね機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   has_many :ramen
+  has_many :likes
   has_many :comments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  def liked_by?(ramen_id)
+    likes.where(ramen_id: ramen_id).exists?
+  end
 end
