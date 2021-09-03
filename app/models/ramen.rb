3a9ebd3ebe960e060114dfc9226c7cb441cd2ec6
keita12 <18b1228@andrew.ac.jp>
2021-09-03 11:10:53 +0900

    いいねされた投稿を削除できるように修正

--- app/models/ramen.rb
+++ app/models/ramen.rb
@@ -1,6 +1,6 @@
 class Ramen < ApplicationRecord
 	belongs_to :user
-	has_many :likes
+	has_many :likes, dependent: :destroy
 	has_many :comments, dependent: :destroy
 	has_one_attached :image
 	def self.search(search)
