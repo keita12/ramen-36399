456f452ec4642f7a8ce8d4a0b8c8fc89700acf21
keita12 <18b1228@andrew.ac.jp>
2021-09-03 17:50:11 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
-  resources :users, only: :show
+  resources :users, only: :show do
+    collection do
+      get :likes
+    end
+  end
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
+
 end
